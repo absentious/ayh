66335ba7d49bf3634fc95f2a5ac76791cacfbbbc
austin hou <absentious@austins-MacBook-Pro.local>
2018-07-22 17:46:42 -0700

    music mobile formatting

--- src/components/subpages/Albums.js
+++ src/components/subpages/Albums.js
@@ -15,28 +15,28 @@ class Albums extends Component {
     }
     
     render () {
-    console.log(listData);
-    return (
-        <div class='subcontent'>
-            <p class='t_body2 bottomSpace'>i spend a lot of time listening to music of all genres. here is a collection of albums i enjoy. </p>
-            
-            {listData.sections.map(item => {
-                return(
-                    <div class='bottomSpace'>
-                        <div class='bottomSpace_half'>
-                            <p>
-                                <span class='t_stitle nav_symbol'>{item.label}</span>
-                            </p>
+        console.log(listData);
+        return (
+            <div class='subcontent'>
+                <p class='t_body2 bottomSpace'>i spend a lot of time listening to music of all genres. here is a collection of albums i enjoy. </p>
+                
+                {listData.sections.map(item => {
+                    return(
+                        <div class='bottomSpace'>
+                            <div class='bottomSpace_half'>
+                                <p>
+                                    <span class='t_stitle nav_symbol m_noleft'>{item.label}</span>
+                                </p>
+                            </div>
+                            <Albums_AlbumList albumList={item.albumList} />
                         </div>
-                        <Albums_AlbumList albumList={item.albumList} />
-                    </div>
-                )
-            })}
+                    )
+                })}
 
 
-        </div>
+            </div>
 
-    )
+        )
     }
 }
 
